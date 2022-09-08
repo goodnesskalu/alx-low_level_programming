25eb21c60013712a991531f99ee414df397b0bc9
GoodnessKalu <kalugoodnessi@gmail.com>
2022-09-08 11:43:01 -0700

    C Language

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/* Author: Goodness Kalu
+ * Date: 09/08/2022
+ * Task: Implement puts function */
+
+#include <stdio.h>
+int (void) 
+{
+	puts ("Programming is like building a multilingual puzzle");
+	return 0;
+}
