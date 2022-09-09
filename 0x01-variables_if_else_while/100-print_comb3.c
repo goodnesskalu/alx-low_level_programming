88e25ecbefffb6f789d49cf3f679a42be3992ae3
GoodnessKalu <kalugoodnessi@gmail.com>
2022-09-09 07:37:19 -0700

    C Language

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of two digits
+ * should not be the same
+ * Return: 0 is success
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
