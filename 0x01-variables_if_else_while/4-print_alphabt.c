b3e6d1b4a2e80658c35744ec365150cdbfbff468
GoodnessKalu <kalugoodnessi@gmail.com>
2022-09-09 06:42:51 -0700

    C Language

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabets in lower case
+ * except e and q
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
