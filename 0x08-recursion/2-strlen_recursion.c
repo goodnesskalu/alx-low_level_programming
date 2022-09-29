cabb1acdbbd1707c90d39ffb3d32bbe32589dd2b
goodnesskalu <kalugoodnessi@gmail.com>
2022-09-29 11:11:54 +0000

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+nclude "main.h"
+/**
+ * _strlen_recursion - prints length of string
+ * @s: string to be calculated
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
