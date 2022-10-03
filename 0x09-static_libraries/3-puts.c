b83e652b93b36490058a48e00f279901cb0d2291
goodnesskalu <kalugoodnessi@gmail.com>
2022-10-03 11:11:29 +0000

    static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
